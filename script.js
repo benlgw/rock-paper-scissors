db5761cfe3e3c78a2bce9e8b8f54d18f80351c1f
benlgw <ben.greaveswarwick@tutamail.com>
2026-05-27 20:32:10 +0100

    refactored getComputerChoice to use swicth/case instead of multiple if statements

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ let computerScore = 0;
 function getComputerChoice(a) {
 	let randomNum = Math.floor(Math.random() * 3);
 
-	if (randomNum == 0) {
-		return "rock";
-	} else if (randomNum == 1) {
-		return "paper";
-	} else if (randomNum == 2) {
-		return "scissors";
+	switch (randomNum) {
+		case 0:
+			return "rock";
+			break;
+		case 1:
+			return "paper";
+			break;
+		case 2:
+			return "scissors";
+			break;
 	}
 }
 
