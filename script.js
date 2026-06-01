2d69e399fa22eac0ababd27491049272fc4ba20a
benlgw <ben.greaveswarwick@tutamail.com>
2026-06-01 14:35:07 +0100

    added a reset button to play again

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
-let humanScore = 0;
+const body = document.querySelector("body");
+
+const roundCounter = document.querySelector("#round");
+const scoreCounter = document.querySelector("#score");
+const winner = document.querySelector("#winner");
+
+const buttons = document.querySelectorAll("button");
+const result = document.querySelector("#result");
+
+const resetButton = document.createElement("button");
+resetButton.textContent = "New Game";
+
+let playerScore = 0;
 let computerScore = 0;
 
-function getComputerChoice(a) {
+let round = 1;
+
+buttons.forEach((element) => {
+	element.addEventListener("click", (event) => {
+		const humanChoice = element.textContent.toLowerCase();
+		const computerChoice = getComputerChoice();
+
+		if (round <= 5) {
+			playRound(humanChoice, computerChoice);
+			round += 1;
+		}
+
+		if (round > 5) {
+			winner.textContent =
+				playerScore > computerScore ? "You won! :)" : "You lose! :(";
+
+			body.appendChild(resetButton);
+
+			resetButton.addEventListener("click", () => {
+				body.removeChild(resetButton);
+
+				round = 1;
+				roundCounter.textContent = "Round: 1";
+
+				playerScore = 0;
+				computerScore = 0;
+				scoreCounter.textContent = "Player: 0 | Computer: 0";
+			});
+		} else {
+			roundCounter.textContent = `Round: ${round}`;
+		}
+	});
+});
+
+function getComputerChoice() {
 	let randomNum = Math.floor(Math.random() * 3);
 
 	switch (randomNum) {
@@ -17,15 +63,13 @@ function getComputerChoice(a) {
 	}
 }
 
-function getHumanChoice() {
-	let humanPick = prompt("Rock, Paper, or Scissors? : ").toLowerCase();
-	return humanPick;
-}
-
 function playRound(humanChoice, computerChoice) {
 	// Draw
 	if (humanChoice == computerChoice) {
-		console.log("It's a Draw!");
+		result.textContent = "It's a Draw!";
+		playerScore += 1;
+		computerScore += 1;
+		scoreCounter.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
 	}
 	// Win
 	if (
@@ -33,7 +77,9 @@ function playRound(humanChoice, computerChoice) {
 		(humanChoice == "paper" && computerChoice == "rock") ||
 		(humanChoice == "scissors" && computerChoice == "paper")
 	) {
-		console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+		result.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+		playerScore += 1;
+		scoreCounter.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
 	}
 	// Lose
 	if (
@@ -41,24 +87,8 @@ function playRound(humanChoice, computerChoice) {
 		(humanChoice == "paper" && computerChoice == "scissors") ||
 		(humanChoice == "scissors" && computerChoice == "rock")
 	) {
-		console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+		result.textContent = `You Lose! ${computerChoice} beats ${humanChoice}`;
+		computerScore += 1;
+		scoreCounter.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
 	}
-}
-
-function playGame() {
-	let round = 1;
-	while (round <= 5) {
-		const humanChoice = getHumanChoice();
-		const computerChoice = getComputerChoice();
-		playRound(humanChoice, computerChoice);
-		round += 1;
-	}
-}
-
-playGame();
-
-if (humanScore > computerScore) {
-	console.log("You win! :)");
-} else {
-	console.log("You lose! :(");
 }
